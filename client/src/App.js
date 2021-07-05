01941f442000f7e85710985e15eed1ac1cb3a3a1
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-07-05 16:37:08 -0600

    refactor for redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import { Provider } from 'react-redux';
 
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
@@ -9,7 +10,7 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
-import { StoreProvider } from "./utils/GlobalState";
+import store from './redux/store';
 import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
 
@@ -30,7 +31,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -41,7 +42,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
+import { useDispatch, useSelector } from "react-redux";
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
+ 
 
   const {
     image,
@@ -16,7 +16,8 @@ function ProductItem(item) {
     quantity
   } = item;
 
-  const { cart } = state
+  const dispatch = useDispatch();
+  const cart = useSelector(state => state.cart);
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
