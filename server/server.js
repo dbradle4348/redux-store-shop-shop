3d0469262c2a12a05cc4e7f13b86424835235c93
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-07-22 12:10:28 -0600

    added cors package and code to server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const cors = require('cors')
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
@@ -15,9 +16,11 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
