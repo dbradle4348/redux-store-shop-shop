314eab6dccd4e469c13609200b5b5fbbd9acf854
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-07-29 15:33:55 -0600

    redone file structure

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const cors = require('cors')
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
@@ -16,11 +15,9 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
-app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
